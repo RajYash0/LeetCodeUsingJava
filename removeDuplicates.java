9938d9777f778496468f9d3228ef0785ea177e24
Yash Raj <yashhraj3@gmail.com>
2025-04-20 17:00:24 +0530

    Create removeDuplicates.java

---
+++ removeDuplicates.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        Solution solution = new Solution();
+        int k = solution.removeDuplicates(nums);
+        System.out.println("k = " + k);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
