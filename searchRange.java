f88d981a890195273cb98d6bc0c163c377738552
Yash Raj <yashhraj3@gmail.com>
2025-04-28 09:52:37 +0530

    Create searchRange.java

---
+++ searchRange.java
@@ -0,0 +1,37 @@
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        int[] result = solution.searchRange(nums, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+    
+    private int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0, right = nums.length - 1, bound = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                bound = mid;
+                if (isFirst) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return bound;
+    }
+}
