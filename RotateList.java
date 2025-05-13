f292faa5a4cf9667482dec364dc363636a8ab86d
Yash Raj <yashhraj3@gmail.com>
2025-05-13 18:52:19 +0530

    Create RotateList.java

---
+++ RotateList.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+
+        ListNode tail = head;
+        int length = 1;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+
+        k = k % length;
+        if (k == 0) return head;
+
+        tail.next = head;
+
+        int stepsToNewHead = length - k;
+        ListNode newTail = tail;
+        while (stepsToNewHead-- > 0) {
+            newTail = newTail.next;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        RotateList solution = new RotateList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int k = 2;
+        ListNode result = solution.rotateRight(head, k);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
