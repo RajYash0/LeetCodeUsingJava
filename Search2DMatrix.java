76ba6a79793c27eb01c88d0251e93a34b7bc7184
Yash Raj <yashhraj3@gmail.com>
2025-05-19 09:46:48 +0530

    Create Search2DMatrix.java

---
+++ Search2DMatrix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Search2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int val = matrix[mid / n][mid % n];
+            if (val == target) return true;
+            else if (val < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), n = sc.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = sc.nextInt();
+        int target = sc.nextInt();
+        System.out.print(new Search2DMatrix().searchMatrix(matrix, target));
+    }
+}
