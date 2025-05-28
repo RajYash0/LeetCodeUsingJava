3f3d663d84ebf6290fbcefb1bc26eeb9596ada50
Yash Raj <yashhraj3@gmail.com>
2025-05-28 09:15:59 +0530

    Create ReverseLinkedListIII.java

---
+++ ReverseLinkedListIII.java
@@ -0,0 +1,42 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class ReverseLinkedListII {
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) prev = prev.next;
+        ListNode start = prev.next;
+        ListNode then = start.next;
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next;
+            then.next = prev.next;
+            prev.next = then;
+            then = start.next;
+        }
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode result = reverseBetween(head, 2, 4);
+        printList(result); // Output: 1 4 3 2 5
+    }
+}
