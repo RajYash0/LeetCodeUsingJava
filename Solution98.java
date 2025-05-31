29c9064d76e7325a6f867c8dca50af3bdc5dc55d
Yash Raj <yashhraj3@gmail.com>
2025-05-31 12:47:12 +0530

    Create Solution98.java

---
+++ Solution98.java
@@ -0,0 +1,29 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution98 {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode node, Integer lower, Integer upper) {
+        if (node == null) return true;
+        int val = node.val;
+        if (lower != null && val <= lower) return false;
+        if (upper != null && val >= upper) return false;
+        if (!helper(node.right, val, upper)) return false;
+        if (!helper(node.left, lower, val)) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        Solution98 sol = new Solution98();
+        System.out.println(sol.isValidBST(root));
+    }
+}
