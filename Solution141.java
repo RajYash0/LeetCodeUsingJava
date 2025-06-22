c879be31701ee18486030466a927769c41b6e0ae
Yash Raj <yashhraj3@gmail.com>
2025-06-22 12:30:21 +0530

    Create Solution141.java

---
+++ Solution141.java
@@ -0,0 +1,35 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(0);
+        ListNode fourth = new ListNode(-4);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = second; // creates a cycle
+
+        Solution141 sol = new Solution141();
+        System.out.println(sol.hasCycle(head)); // Output: true
+    }
+}
