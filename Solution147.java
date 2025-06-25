e6beefead18616855d3648c61d42e472d5136b17
Yash Raj <yashhraj3@gmail.com>
2025-06-25 13:28:51 +0530

    Create Solution147.java

---
+++ Solution147.java
@@ -0,0 +1,26 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution147 {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode prev = dummy;
+            while (prev.next != null && prev.next.val < curr.val) {
+                prev = prev.next;
+            }
+            ListNode nextTemp = curr.next;
+            curr.next = prev.next;
+            prev.next = curr;
+            curr = nextTemp;
+        }
+        return dummy.next;
+    }
+}
