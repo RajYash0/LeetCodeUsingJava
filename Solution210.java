6512526c8b38bf1838106650f7766ea70d6ee633
Yash Raj <yashhraj3@gmail.com>
2025-07-19 14:22:06 +0530

    Create Solution210.java

---
+++ Solution210.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++)
+            adj.add(new ArrayList<>());
+        for (int[] p : prerequisites) {
+            adj.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                q.offer(i);
+        int[] order = new int[numCourses];
+        int idx = 0;
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            order[idx++] = u;
+            for (int v : adj.get(u)) {
+                if (--indegree[v] == 0)
+                    q.offer(v);
+            }
+        }
+        return idx == numCourses ? order : new int[0];
+    }
+}
