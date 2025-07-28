2b213d54df329c4aba170414714af581018c6dd5
Yash Raj <yashhraj3@gmail.com>
2025-07-28 17:02:43 +0530

    create Solution227.java

---
+++ Solution227.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char sign = '+';
+        s = s.replaceAll(" ", "");
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            if (!Character.isDigit(ch) || i == s.length() - 1) {
+                if (sign == '+') stack.push(num);
+                else if (sign == '-') stack.push(-num);
+                else if (sign == '*') stack.push(stack.pop() * num);
+                else if (sign == '/') stack.push(stack.pop() / num);
+                sign = ch;
+                num = 0;
+            }
+        }
+        int result = 0;
+        for (int n : stack) result += n;
+        return result;
+    }
+}
