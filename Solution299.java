fd242c112d27479ab94815305094393af55205f1
Yash Raj <yashhraj3@gmail.com>
2025-08-15 10:43:11 +0530

    Create Solution299.java

---
+++ Solution299.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+        int[] s = new int[10], g = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else {
+                s[secret.charAt(i) - '0']++;
+                g[guess.charAt(i) - '0']++;
+            }
+        }
+        for (int i = 0; i < 10; i++) cows += Math.min(s[i], g[i]);
+        return bulls + "A" + cows + "B";
+    }
+}
