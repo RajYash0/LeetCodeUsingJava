9d63c5e904d5977bac7ea50dde2407faad41d736
Yash Raj <yashhraj3@gmail.com>
2025-08-19 07:09:10 +0530

    Create Solution310.java

---
+++ Solution310.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) return Collections.singletonList(0);
+        List<Set<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new HashSet<>());
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) if (adj.get(i).size() == 1) leaves.add(i);
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int leaf : leaves) {
+                int neighbor = adj.get(leaf).iterator().next();
+                adj.get(neighbor).remove(leaf);
+                if (adj.get(neighbor).size() == 1) newLeaves.add(neighbor);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
+    }
+}
