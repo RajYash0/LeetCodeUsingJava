804525127b4d081749a8a4c7b942152779ead69d
Yash Raj <yashhraj3@gmail.com>
2025-08-29 16:16:17 +0530

    Create Solution336.java

---
+++ Solution336.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (words == null || words.length < 2) return res;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) map.put(words[i], i);
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j <= words[i].length(); j++) {
+                String left = words[i].substring(0, j);
+                String right = words[i].substring(j);
+                if (isPalindrome(left)) {
+                    String rev = new StringBuilder(right).reverse().toString();
+                    if (map.containsKey(rev) && map.get(rev) != i)
+                        res.add(Arrays.asList(map.get(rev), i));
+                }
+                if (right.length() > 0 && isPalindrome(right)) {
+                    String rev = new StringBuilder(left).reverse().toString();
+                    if (map.containsKey(rev) && map.get(rev) != i)
+                        res.add(Arrays.asList(i, map.get(rev)));
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) if (s.charAt(l++) != s.charAt(r--)) return false;
+        return true;
+    }
+}
