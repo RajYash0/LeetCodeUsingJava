e54de74b5f28382fdae63d7a8b2e5bea8b391e61
Yash Raj <yashhraj3@gmail.com>
2025-08-30 08:34:17 +0530

    Create Solution341.java

---
+++ Solution341.java
@@ -0,0 +1,25 @@
+public class NestedIterator implements Iterator<Integer> {
+    private List<Integer> list = new ArrayList<>();
+    private int index = 0;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        flatten(nestedList);
+    }
+
+    private void flatten(List<NestedInteger> nestedList) {
+        for (NestedInteger ni : nestedList) {
+            if (ni.isInteger()) list.add(ni.getInteger());
+            else flatten(ni.getList());
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return list.get(index++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < list.size();
+    }
+}
