88a5ea09da6df85d1416fb85c3055af0be98fe27
Yash Raj <yashhraj3@gmail.com>
2025-09-04 17:45:30 +0530

    Create Solution354.java

---
+++ Solution354.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int[] dp = new int[envelopes.length];
+        int len = 0;
+        for (int[] env : envelopes) {
+            int i = Arrays.binarySearch(dp, 0, len, env[1]);
+            if (i < 0) i = -(i + 1);
+            dp[i] = env[1];
+            if (i == len) len++;
+        }
+        return len;
+    }
+}
