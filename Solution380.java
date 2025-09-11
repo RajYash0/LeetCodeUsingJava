86e6a055cf7a1c2e9e3e3ec0f9a881ce1390e140
Yash Raj <yashhraj3@gmail.com>
2025-09-11 16:49:04 +0530

    Create Solution380.java

---
+++ Solution380.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class RandomizedSet {
+    Map<Integer, Integer> map;
+    List<Integer> list;
+    Random rand;
+
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        int idx = map.get(val);
+        int last = list.get(list.size() - 1);
+        list.set(idx, last);
+        map.put(last, idx);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
