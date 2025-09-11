a37e2e744f3754562907f92d7103d2bbaee85291
Yash Raj <yashhraj3@gmail.com>
2025-09-11 16:48:27 +0530

    Create Solution378.java

---
+++ Solution378.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0], right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = countLessEqual(matrix, mid, n);
+            if (count < k) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+
+    private int countLessEqual(int[][] matrix, int mid, int n) {
+        int count = 0, row = n - 1, col = 0;
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= mid) {
+                count += row + 1;
+                col++;
+            } else {
+                row--;
+            }
+        }
+        return count;
+    }
+}
