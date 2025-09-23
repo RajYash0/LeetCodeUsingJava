a4194d76d2557412a4ba97fe019387a866297e11
Yash Raj <yashhraj3@gmail.com>
2025-09-23 14:55:42 +0530

    Create Solution403.java

---
+++ Solution403.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int s : stones) map.put(s, new HashSet<>());
+        map.get(0).add(0);
+        for (int s : stones) {
+            for (int k : map.get(s)) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step > 0 && map.containsKey(s + step)) {
+                        map.get(s + step).add(step);
+                    }
+                }
+            }
+        }
+        return !map.get(stones[n - 1]).isEmpty();
+    }
+}
