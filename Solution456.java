a7a29e659931a1fce00f87efbf1ef2d512e7ac26
Yash Raj <166943483+RajYash0@users.noreply.github.com>
2025-10-14 19:02:37 +0530

    Create Solution456.java

---
+++ Solution456.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return false;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int third = Integer.MIN_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < third) return true;
+            while (!stack.isEmpty() && stack.peek() < nums[i]) {
+                third = Math.max(third, stack.pop());
+            }
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
