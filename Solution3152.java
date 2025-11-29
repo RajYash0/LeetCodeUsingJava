c7c672821fe4d7363e0be59540cb1e05b524699c
Yash Raj <166943483+RajYash0@users.noreply.github.com>
2025-11-29 15:55:28 +0530

    Create Solution3152.java

---
+++ Solution3152.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for (int x : nums) sum += x;
+        return sum % k;
+    }
+}
