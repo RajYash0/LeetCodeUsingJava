40c1828b51c3f02b7392837d042b8f17bd028ff1
Yash Raj <166943483+RajYash0@users.noreply.github.com>
2025-12-05 11:07:59 +0530

    Create Solution3432.java

---
+++ Solution3432.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int countPartitions(int[] nums) {
+        long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        if (totalSum % 2 == 0) {
+            return nums.length - 1;
+        } else {
+            return 0;
+        }
+    }
+}
