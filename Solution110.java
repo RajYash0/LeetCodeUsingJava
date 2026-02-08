77175a93c758e79b975f8fca6d3605346cf8dbe3
Yash Raj <166943483+RajYash0@users.noreply.github.com>
2026-02-08 23:08:41 +0530

    Update Solution110.java

--- Solution110.java
+++ Solution110.java
@@ -1,32 +1,23 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+class Solution {
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = dfs(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = dfs(root.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
     }
-}
 
-class Solution110 {
     public boolean isBalanced(TreeNode root) {
-        return checkHeight(root) != -1;
-    }
-    
-    private int checkHeight(TreeNode node) {
-        if (node == null) return 0;
-        
-        int left = checkHeight(node.left);
-        if (left == -1) return -1;
-        
-        int right = checkHeight(node.right);
-        if (right == -1) return -1;
-        
-        if (Math.abs(left - right) > 1) return -1;
-        
-        return Math.max(left, right) + 1;
+        return dfs(root) != -1;
     }
 }
