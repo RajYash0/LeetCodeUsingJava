5e809d0221a8f44e5c040536e531e5e4eab74023
Yash Raj <166943483+RajYash0@users.noreply.github.com>
2026-02-17 20:09:37 +0530

    Update Solution401.java

--- Solution401.java
+++ Solution401.java
@@ -1,15 +1,23 @@
-import java.util.*;
-
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
-        List<String> res = new ArrayList<>();
-        for (int h = 0; h < 12; h++) {
-            for (int m = 0; m < 60; m++) {
-                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
-                    res.add(String.format("%d:%02d", h, m));
+        List<String> result = new ArrayList<>();
+        
+        for (int hour = 0; hour < 12; hour++) {
+            
+            for (int minute = 0; minute < 60; minute++) {
+                
+                int totalBits = Integer.bitCount(hour) + Integer.bitCount(minute);
+                
+                if (totalBits == turnedOn) {
+                    
+                    String time = hour + ":" + 
+                                  (minute < 10 ? "0" + minute : minute);
+                    
+                    result.add(time);
                 }
             }
         }
-        return res;
+        
+        return result;
     }
 }
